dc09174ff188846898808fb977bc55c1692860ee
Sourabh Rai <77492727+SourabhRai@users.noreply.github.com>
2023-06-09 14:47:14 +0530

    Beautified the code
    (https://codebeautify.org/react-formatter#)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,72 @@
-import React from 'react';
-const Login = (props) => 
-{
-const { email, setEmail, password, setPassword, handleLogin, handleSignup, hasAccount, setHasAccount, emailError, passwordError } = props;
-return(
+import React from "react";
+const Login = (props) => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError,
+  } = props;
+  return (
     <section className="login">
-    <div className="loginContainer">
-    <label>Username</label>
-    <input
-    type="text"
-    autoFocus
-    required
-    value ={email}
-    onChange= {(e) => setEmail(e.target.value)}/>
-    
-    <p className="errorMsg">{emailError}</p>
-    <label>Password</label>
-    <input
-    type="password"
-    required
-    value = {password}
-    onChange = {(e) => setPassword(e.target.value)}
-    />
-    <p className="errorMsg">{passwordError}</p>
+      <div className="loginContainer">
+        <label>Username</label>
+        <input
+          type="text"
+          autoFocus
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-    <div className="btnContainer">
-        {
-        hasAccount ? 
-        (
-            <>
-            <button onClick={handleLogin}>Sign in</button>
-            <p>Don't have an account ? <span onClick={()=>{setHasAccount(!hasAccount)}}>Sign up</span></p>
-            </>
-        ):
-        (
-            <>
-            <button onClick={handleSignup}>Sign up</button>
-            <p>Have an account ? <span onClick={()=>{setHasAccount(!hasAccount)}} >Sign in</span></p>
-            </>
-        )
-        }
-    </div>
+        <p className="errorMsg">{emailError}</p>
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="errorMsg">{passwordError}</p>
 
-    </div>
+        <div className="btnContainer">
+          {hasAccount ? (
+            <>
+              <button onClick={handleLogin}>Sign in</button>
+              <p>
+                Don't have an account ?{" "}
+                <span
+                  onClick={() => {
+                    setHasAccount(!hasAccount);
+                  }}
+                >
+                  Sign up
+                </span>
+              </p>
+            </>
+          ) : (
+            <>
+              <button onClick={handleSignup}>Sign up</button>
+              <p>
+                Have an account ?{" "}
+                <span
+                  onClick={() => {
+                    setHasAccount(!hasAccount);
+                  }}
+                >
+                  Sign in
+                </span>
+              </p>
+            </>
+          )}
+        </div>
+      </div>
     </section>
-)
-}
+  );
+};
 export default Login;
